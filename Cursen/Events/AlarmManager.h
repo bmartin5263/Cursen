f1524a0749bca2de10e39dd97416a87737111d79
Brandon Martin <bsm2112@yahoo.com>
2019-04-04 14:40:32 -0500

    Added documentation

--- Cursen/Events/AlarmManager.h
+++ Cursen/Events/AlarmManager.h
@@ -20,19 +20,38 @@ public:
     typedef std::function<void()> VoidFunc;
 
     /**
-     * \brief Iterate through the alarms and call their interval functions if ready
+     * @brief Iterate through the alarms and call their interval functions if ready
      */
     static void ProcessAlarms() { Instance().privProcessAlarms(); }
 
     /**
-     * \brief Create a new alarm for a specified component
+     * @brief Create a new Alarm for a specified component
+     *
+     * @param component Component to register
+     * @param interval_function Callback for each interval
+     * @param seconds Seconds between each callback
      */
     static void StartAlarm(Component *component, VoidFunc interval_function, double seconds)
     {
         Instance().privStartAlarm(component, interval_function, seconds);
     };
 
+    /**
+     * @brief Stops and removes an Alarm from the update list
+     *
+     * @param component Component to deregister
+     */
     static void StopAlarm(Component *component) { Instance().privStopAlarm(component); };
+
+    /**
+     * @brief Create a new Alarm that will automatically cancel itself after a specified time
+     *
+     * @param component Component to register
+     * @param interval_function Callback for each interval
+     * @param seconds Seconds between each callback
+     * @param total_time Seconds before Alarm cancels itself
+     * @param cancel_function Optional callback for when the Alarm is cancelled
+     */
     static void StartAutoAlarm(Component *component, VoidFunc interval_function, double seconds, double total_time,
                                VoidFunc cancel_function = Alarm::VOID)
     {
@@ -40,6 +59,10 @@ public:
     }
 
 private:
+
+    typedef std::queue<Alarm*> AlarmQueue;
+    typedef std::queue<Component*> ComponentQueue;
+    typedef std::unordered_map<Component*, Alarm*> AlarmMap;
 
     void privStartAlarm(Component *component, VoidFunc f, double seconds);
     void privStartAutoAlarm(Component *component, VoidFunc f, double seconds, double total_time, VoidFunc cf);
@@ -49,9 +72,9 @@ private:
     void handleStopRequests();
     void handleStartRequests();
 
-    std::queue<Alarm*> startRequests;
-    std::queue<Component*> stopRequests;
-    std::unordered_map<Component*, Alarm*> alarms;
+    AlarmQueue startRequests;       /// Queue for Component's requests to start Alarms
+    ComponentQueue stopRequests;    /// Queue for Component's requests to cancel Alarms
+    AlarmMap alarms;                /// Map Component* -> Alarm*
     std::chrono::system_clock::time_point lastUpdate;
 
     static AlarmManager* instance;
